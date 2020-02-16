71bad4dc8a4aa8a4baf4667d8db9a96ccaf0c2d1
Antxon Eguiguren <antxonegi@gmail.com>
2020-02-16 14:10:40 +0100

    Bootstrap instalado, componentes y servicio creados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    FormularioComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
